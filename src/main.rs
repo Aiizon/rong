d8bdb4a014c92358014fa5b959fcc90674f63119
Aizon <killian.bonneau@iia-formation.fr>
2025-05-29 20:40:10 +0200

    Add paddles

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,63 @@
 use bevy::prelude::*;
 
+const PADDLE_SIZE: Vec2 = Vec2::new(10.0, 100.0);
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_systems(Startup, (
+            spawn_camera, 
+            spawn_players
+        ))
         .run()
     ;
+}
+
+#[derive(Component)]
+struct Paddle {
+    move_up:   KeyCode,
+    move_down: KeyCode
+}
+
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn(Camera2d::default());
+}
+
+fn spawn_players(mut commands: Commands, query: Query<&Window>) {
+    let window = query.single().expect("No primary window found");
+    let width = window.width() / 2.0;
+    
+    commands.spawn_batch([
+        (
+            Sprite {
+                color: Color::WHITE,
+                custom_size: Some(PADDLE_SIZE),
+                ..default()
+            },
+            Transform {
+                translation: Vec3::new(-(width - 15.0), 0.0, 0.0),
+                ..default()
+            },
+            Paddle {
+                move_up:   KeyCode::KeyZ,
+                move_down: KeyCode::KeyS,
+            },
+        ),
+        (
+            Sprite {
+                color: Color::WHITE,
+                custom_size: Some(PADDLE_SIZE),
+                ..default()
+            },
+            Transform {
+                translation: Vec3::new((width - 15.0), 0.0, 0.0),
+                ..default()
+            },
+            Paddle {
+                move_up:   KeyCode::ArrowUp,
+                move_down: KeyCode::ArrowDown,
+            },
+        ),
+    ]);
 }
