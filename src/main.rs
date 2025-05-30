1bfb1e97df91a19acdc74350dcd90ca7f24a2e18
Aizon <killian.bonneau@iia-formation.fr>
2025-05-30 09:56:21 +0200

    Window settings

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,22 @@ mod paddle;
 mod ball;
 
 use bevy::prelude::*;
+use bevy::window::WindowResolution;
+
+const WINDOW_WIDTH:  f32 = 1280.0;
+const WINDOW_HEIGHT: f32 = 720.0;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                resizable: false,
+                title: "Rong".into(),
+                resolution: WindowResolution::new(WINDOW_WIDTH, WINDOW_HEIGHT),
+                ..default()
+            }),
+            ..default()
+        }))
         .add_systems(Startup, (
             spawn_camera, 
             paddle::spawn_players,
